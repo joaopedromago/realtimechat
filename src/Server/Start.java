be8c2437359dd10ee2c513ad3f9bd16eb2dc5bd4
João Mago <joao.mago@drlavatudo.com>
2019-11-10 23:02:33 -0300

    creating server class

---
+++ src/Server/Start.java
@@ -0,0 +1,20 @@
+// package Server;
+
+// import javax.naming.NamingException;
+
+// public class Start {
+
+// public static void main(String[] args) {
+// }
+
+// public static Context getInitialContext() throws JMSException,
+// NamingException {
+// Propeties props = new Propeties();
+// props.setPropriety("", "");
+// props.setPropriety("", "");
+// props.setPropriety("", "");
+// Context context = new InitialContext(props);
+// return context;
+// }
+
+// }
